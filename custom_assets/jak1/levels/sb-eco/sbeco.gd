7b395673a0e508892bdaffc9fd130cd4a8258143
unknown <Crashday20@gmx.de>
2024-09-07 11:04:20 +0200

    added level, including textures

--- custom_assets/jak1/levels/sb-eco/sbeco.gd
+++ custom_assets/jak1/levels/sb-eco/sbeco.gd
@@ -4,5 +4,10 @@
 ;; the actual file name still needs to be 8.3
 ("SBE.DGO"
  ("static-screen.o"
+  "water-anim-jungle-ag.go"
+  "water-anim-training-ag.go"
+  "water-anim-sunken-ag.go"
+  "sunken-water.o"
+  "jungle-obs.o"
   "sb-eco.go"
 ))
